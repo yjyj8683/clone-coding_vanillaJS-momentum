0e0bcf297be6f6cb8582505eb2d2c28c22a2d3e8
yjyj8683 <yjyj8683@naver.com>
2022-02-04 17:31:13 +0900

    2.6 강의 완료 : Objects

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert('hi!');
+// variable
+const playerName = "sy";
+const playerPoints = 9999;
+
+// array [] 대괄호 사용
+const player1 = ["sy", 9999];
+
+// object : {} 중괄호 사용
+// object는 property라는 속성을 넣을 수 있음
+// 설명이 필요할 때 사용
+// const로 만든 함수가 player라는 object 타입이므로 안의 속성이 바뀌는 건 상관 없음
+const player = {
+    name: "sy",
+    potins: 9999,
+};
+console.log(player);
+console.log(player.name);
+
+// ~. 의 다음은 function 임
